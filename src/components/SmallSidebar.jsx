2a99ce7e57949d039d1f8d63b40a98fa925ad240
JanMajer86 <majer.jan.1986@gmail.com>
2025-02-16 20:50:41 +0100

    logout container

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -2,13 +2,11 @@ import Wrapper from "../assets/wrappers/SmallSidebar";
 import { FaTimes } from "react-icons/fa";
 
 import Logo from "./Logo";
-import { NavLink } from "react-router-dom";
-import links from "../utils/links";
 import { useDashboardContext } from "../pages/DashboardLayout";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
 	const { showSidebar, toggleSidebar } = useDashboardContext();
-	console.log(showSidebar, toggleSidebar);
 
 	return (
 		<Wrapper>
@@ -24,23 +22,7 @@ const SmallSidebar = () => {
 					<header>
 						<Logo />
 					</header>
-					<div className="nav-links">
-						{links.map((link) => {
-							const { text, path, icon } = link;
-							return (
-								<NavLink
-									to={path}
-									key={text}
-									className="nav-link"
-									onClick={toggleSidebar}
-									end
-								>
-									<span className="icon">{icon}</span>
-									{text}
-								</NavLink>
-							);
-						})}
-					</div>
+					<NavLinks />
 				</div>
 			</div>
 		</Wrapper>
